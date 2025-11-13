2f6c77d569638d65d3403be9cf1d2b62b6714a24
afsanehghasedi93-prog <afsanehghasedi93@gmail.com>
2025-11-13 03:28:50 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using interfas;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace interfas
+{
+   public class program
+    {
+        public static void Main(string[] args)
+        {
+            List<ianimal> animals = new List<ianimal>
+            {
+new dog(),
+            new Cat(),
+new Bird(),
+          new Fish(),
+            } ;
+            Console.WriteLine("animal shelter daily routine");
+            foreach (ianimal animal in animals)
+            {
+                Console.WriteLine($"\n**processing:{animal.GetType().Name}**");
+               ianimal.walk();
+              ianimal.Eat();
+               ianimal.MakeSound();
+              ianimal.Sleep();
+            }
+            Console.WriteLine("\n---daily routine completed---");
+            Console.ReadKey();
+        }
+    }
+    
+    
+    }
+
---
+++ cat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using interfas;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfas
+{
+    public class Cat
+    {
+        public void walk()
+        {
+            Console.WriteLine("cat is walking silently and gracefully");
+        }
+        public void Eat()
+        {
+            Console.WriteLine("cat is delicataely sipping milk");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine("cat is sleeping on a sunny spot, purring softly");
+        }
+        public void Makesound()
+        {
+            Console.WriteLine("meow...purrrr...(mews and purrs)");
+        }
+    }
+    class cat
+    {
+    }
+}
